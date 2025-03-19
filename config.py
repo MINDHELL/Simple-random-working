52304e7002374c2fb6eea8757bcfd2f705f086bc
MINDHELL <Tgpurpose640@gmail.com>
2025-03-19 09:35:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os 
 
-API_ID = "your_api_id"
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
+API_ID = "27788368"
+API_HASH = "9df7e9ef3d7e4145270045e5e43e1081"
+BOT_TOKEN = "7692429836:AAHyUFP6os1A3Hirisl5TV1O5kArGAlAEuQ"
 
 # MongoDB settings
-MONGO_URI = "your_mongo_uri"
-DATABASE_NAME = "random_video_bot"
+MONGO_URI = "mongodb+srv://aarshhub:6L1PAPikOnAIHIRA@cluster0.6shiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_NAME = "xyz"
 
 # Owner/Admins
-OWNER_ID = 123456789  # Your Telegram ID
+OWNER_ID = int("6860316927")  # Your Telegram ID
 ADMINS = [OWNER_ID]  # Add more admins if needed
 
 # Quota settings
@@ -17,7 +17,7 @@ MAX_VIDEOS_PER_6HRS = 20
 REFERRALS_NEEDED_FOR_EXTRA_VIDEOS = 5
 
 # Force Subscription
-FORCE_SUB_CHANNEL = "your_channel_username"
+FORCE_SUB_CHANNEL = "https://t.me/test89p"
 
 # Cache settings
 CACHE_TTL = 3600  # Cache expires in 1 hour
