0f27883ce5f7bca0ffc7fec356c98461dbef23af
MINDHELL <Tgpurpose640@gmail.com>
2025-03-19 18:31:57 +0530

    Create index.py

---
+++ handlers/index.py
@@ -0,0 +1,12 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from database.video_management import index_video
+
+@Client.on_message(filters.command("index") & filters.user(OWNER_ID))
+async def handler(client, message: Message):
+    if message.video:
+        video_file_id = message.video.file_id
+        response = index_video(video_file_id)
+        await message.reply_text(response)
+    else:
+        await message.reply_text("Please send a video with this command!")
