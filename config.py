bd539aa0836f6ad8bafdc4b9c97b8f633645d5c9
MINDHELL <Tgpurpose640@gmail.com>
2025-03-19 19:47:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 API_ID = int(os.getenv("API_ID", "27788368"))
 API_HASH = os.getenv("API_HASH", "9df7e9ef3d7e4145270045e5e43e1081")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7692429836:AAHyUFP6os1A3Hirisl5TV1O5kArGAlAEuQ")
 
-MONGO_URI = os.getenv("MONGO_URI", "YOUR_MONGO_URI")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://aarshhub:6L1PAPikOnAIHIRA@cluster0.6shiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = "VideoBotDB"
 COLLECTION_NAME = "Videos"
 
