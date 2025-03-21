9c24f26f7bbeeb73a3e6470a36c5f917fd6b25f9
MINDHELL <Tgpurpose640@gmail.com>
2025-03-21 13:51:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ async def start_web_server():
 def add_handlers():
     try:
         bot.add_handler(start_command_handler)
-        bot.add_handler(random_video_handler)
+        bot.add_handler(random_video.handler)
         bot.add_handler(index.handler)
         bot.add_handler(delete_video_handler)
         bot.add_handler(stats_handler)
