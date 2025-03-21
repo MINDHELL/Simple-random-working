474d2c4b2787a5704df13b081b841789aee06c89
MINDHELL <Tgpurpose640@gmail.com>
2025-03-21 13:41:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,13 +33,13 @@ async def start_web_server():
 def add_handlers():
     try:
         bot.add_handler(start_command_handler)
-        bot.add_handler(random_video.handler)
-        bot.add_handler(index.handler)
-        bot.add_handler(delete_video.handler)
+        bot.add_handler(random_video_handler)
+        bot.add_handler(index_handler)
+        bot.add_handler(delete_video_handler)
         bot.add_handler(stats_handler)
-        bot.add_handler(quota.handler)
-        bot.add_handler(my_plan.handler)
-        bot.add_handler(broadcast.handler)
+        bot.add_handler(quota_handler)
+        bot.add_handler(my_plan_handler)
+        bot.add_handler(broadcast_handler)
         logging.info("✅ Handlers added successfully!")
     except Exception as e:
         logging.error(f"❌ Error adding handlers: {e}")
